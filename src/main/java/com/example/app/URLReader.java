d7dc31da8afb295325764e34e6c5b5e2fc53ada3
Alejandro Anzola Avila <anzola95avila@gmail.com>
2018-03-23 14:19:32 -0500

    agrega clase de URLReader que consulta las paginas / /hello /db

---
+++ src/main/java/com/example/app/URLReader.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author 2110137
+ */
+public class URLReader {
+
+    public static final String BASE_URL = "https://quiet-atoll-31438.herokuapp.com";
+    
+    private static String response(String url) throws Exception {
+        StringBuilder res = new StringBuilder();
+        URL google = new URL(url);
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(google.openStream()))) {
+            String inputLine = null;
+            while ((inputLine = reader.readLine()) != null) {
+                res.append(inputLine);
+                //System.out.println(inputLine);
+            }
+        } catch (IOException x) {
+            System.err.println(x);
+        }
+        
+        return res.toString();
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        List<String> terms = new ArrayList<>(Arrays.asList("/", "/hello", "/db"));
+        
+        for (String term : terms) {
+            System.out.println(response(BASE_URL + term));
+        }
+    }
+
+}
